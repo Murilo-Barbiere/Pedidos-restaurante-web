d0242f4dda4f42d8eba1c59494be9cd468b6115c
Murilo-Barbiere <murilobarbiere@gmail.com>
2026-01-26 10:05:06 -0400

    first commit

---
+++ src/main/java/com/murilo_dev/system_pedidos/SystemPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.murilo_dev.system_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murilo_dev/system_pedidos/SystemPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilo_dev.system_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
